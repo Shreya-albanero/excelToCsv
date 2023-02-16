5f8f2afc12f76c997057894f9447f777390ec3d6
shreya-albanero <shreya.neema@albanero.io>
2023-02-16 12:52:56 +0530

    concat

--- src/main/java/com/example/excelToCsv/controller/CsvController.java
+++ src/main/java/com/example/excelToCsv/controller/CsvController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
-import java.io.FileWriter;
+import org.apache.spark.sql.functions.*;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,7 +21,7 @@ import java.nio.file.Paths;
 @RestController
 public class CsvController {
     SparkSession spark = SparkSession.builder().master("local").getOrCreate();
-
+//============== Excel to CSV ===================================================================================================
     @PostMapping("/file")
     public ResponseEntity<String> file(@RequestParam("file") MultipartFile Webfile) throws IOException {
 
@@ -30,8 +30,6 @@ public class CsvController {
         Files.write(path, bytes);
         String file="/home/albanero/Documents/file.xlsx";
 
-
-        //SparkSession spark = SparkSession.builder().master("local").getOrCreate();
         Dataset<Row> df  = spark.read().format("com.crealytics.spark.excel")
                 .option("header", "true")
                 .option("inferSchema", "true").load(file);
@@ -41,12 +39,22 @@ public class CsvController {
         spark.stop();
         return ResponseEntity.ok("================================= file received ====================");
     }
-
+//============== Changing headers ================================================================================================
     @PostMapping("/headers")
     public ResponseEntity<String> headers(@RequestParam("name") String name, @RequestParam("rename") String rename) {
         Dataset<Row> df=spark.read().option("header", "true").csv("output.csv");
         df=df.withColumnRenamed(name,rename);
         df.show();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+//============== Concatenation of two column values ================================================================================
+
+    @PostMapping("/concat")
+    public ResponseEntity<String> concatenation(@RequestParam("col1") String col1, @RequestParam("col2") String col2){
+        Dataset<Row> df = spark.read().option("header", "true").format("csv").csv("output.csv");
+        df=df.withColumn("concatCol", functions.concat_ws("-",df.col(col1), df.col(col2)));
+        df.write().format("csv").save("concat.csv");
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 }
